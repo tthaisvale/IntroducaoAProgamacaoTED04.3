238ebce5bf60566a906c51fd0349ec7636a2a4a7
Thais Vale <126836029+tthaisvale@users.noreply.github.com>
2023-03-26 20:10:38 -0300

    Add files via upload

---
+++ TED 04 - Questão 01.py
@@ -0,0 +1,5 @@
+valor = int(input("Digite um valor: "))
+if (valor >=0):
+ print("Você digitou um valor positivo!")
+else:
+ print("Você digitou um valor negativo!")
---
+++ TED 04 - Questão 02.py
@@ -0,0 +1,11 @@
+valordamaca=""
+quantidademaca= int(input("Digite a quantidade: "))
+if quantidademaca >= 12:
+ valordamaca= 1
+ resultado=quantidademaca*valordamaca
+ print(f"Você comprou {quantidademaca} maças irá pagar {resultado}
+reais")
+else:
+ valordamaca = 1.30
+ resultado = quantidademaca * valordamaca
+ print(f"Você comprou {quantidademaca} irá pagar {resultado} reais")
